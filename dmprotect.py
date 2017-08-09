d80792012bcad17064f77bf4a581fc84b58b2ee0
Bluscream <admin@timo.de.vc>
2017-08-09 16:30:51 +0200

    more improvements

--- dmprotect.py
+++ dmprotect.py
@@ -3,27 +3,26 @@ from discord.ext import commands
 from cogs.utils.checks import *
 
 class DMProtect:
-    def __init__(self):
-        self.active = False
+    def __init__(self, bot):
+        self.active = True
         self.allowed = False
         self.invites = ['discord.gg', 'discord.io', 'discordapp.com/invite']
         self.bot = bot
 
     async def on_message(self, message):
-        if not self.active: 
-            return
-        if not isinstance(message.channel, discord.abc.PrivateChannel): 
-            return
-        if self.allowed: 
+        if not self.active: return
+        if message.author.id == self.bot.user.id: return
+        if not isinstance(message.channel, discord.abc.PrivateChannel): return
+        if self.allowed:
             self.allowed = False
             return
         messages = await message.channel.history(limit=2).flatten()
-        if not len(messages) == 1: 
-            return
+        if not len(messages) == 1: return
         for invite in self.invites:
             if invite in message.content:
-                message.author.block()
-                print('Automatically blocked user {} for sending an invite link ({}) in their first private message to you.'.format(str(message.author), invite))
+                await message.ack()
+                await message.author.block()
+                print('Automatically blocked user {} for sending an invite link ({}) in their first private message to you.'.format(message.author, invite))
                 return
 
     @commands.command(aliases=['dmp'], pass_context=True)
